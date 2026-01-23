abe8f7bf078527f50fd1222ab50f8af749ecc4fa
Vladislav <bladforceone@gmail.com>
2026-01-23 04:31:18 +0300

    Ensure AuthService interface compliance in service implementation

--- internal/service/auth/service.go
+++ internal/service/auth/service.go
@@ -3,10 +3,14 @@ package auth
 import (
 	"casino_backend/internal/config"
 	"casino_backend/internal/repository"
+	"casino_backend/internal/service"
 
 	"github.com/avito-tech/go-transaction-manager/trm/v2"
 	"github.com/google/uuid"
 )
+
+// Проверка соответствия интерфейсу
+var _ service.AuthService = (*serv)(nil)
 
 type serv struct {
 	txManager trm.Manager
